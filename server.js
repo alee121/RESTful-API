ec72c849dcb6d95d2b1e9b80618fdb8db10b9b05
Adrian <root@localhost.localdomain>
2019-09-05 15:21:59 -0400

    Updating files, installed dependencies, created working basic REST server

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 let express = require('express');
+let http = require('http');
+let bodyparser = require('body-parser');
+let routes = require('./api/routes');
 
-let app = express();
+const app = express();
 
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:false}));
 
+app.use('/routes',routes);
 
-let port = 4250;
+const port = 4250;
 
-app.listen(port);
+const server = http.createServer(app);
+server.listen(port);
 
